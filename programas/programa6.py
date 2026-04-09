c88d6bcaf0c3554050a37a51833daeb1e398673c
Facundo Leite <facundo.leite@fing.edu.uy>
2026-04-09 11:47:57 -0300

    falta terminar porg6, salidas esperadas 6_1 y 6_3 actualizadas

--- programas/programa6.py
+++ programas/programa6.py
@@ -1,15 +1,33 @@
 # -*- coding: utf-8 -*-
 import re
 import sys
+from programa4 import programa4
+from programa5 import programa5
+from programa2 import programa2
 
 def programa6(RutaPdf,RutaXML):
     text = ""
     '''
     SU CÓDIGO
     '''
-    return text
- 
+    xml = programa4(RutaXML)
 
+    if not programa5(RutaPdf,RutaXML):
+        return xml
+    fecha, monto = programa2(RutaPdf)
+
+    #Nos interesa encontrar algo del tipo: Movimiento ... Importe ... Fecha ...
+    movimientos = re.findall(r'<[^>]*Movimiento[^>]*/>', xml)
+
+    #mov es el movimiento que buscamos
+    mov = re.search(r'<[^>]*Movimiento[^>]*Importe\s*=\s*"' + monto + r'"\s*Fecha\s*=\s*"' + fecha + r'"[^>]*/>', xml)
+
+
+    xml = xml.replace(mov.group(0), '')
+    
+
+
+    return xml
 if __name__ == '__main__':
     entrada_pdf = sys.argv[1]  # archivo entrada (param)
     entrada_xml = sys.argv[2]  # archivo entrada (param)
